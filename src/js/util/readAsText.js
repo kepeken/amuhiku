6d0543dbe0ba9faa0081069c49d73e5e0308a132
kepeken <tembin0925@gmail.com>
2019-02-25 03:39:18 +0900

    readAsText 移植

--- src/js/util/readAsText.js
+++
@@ -1,8 +0,0 @@
-export default function readAsText(blob) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = () => reject(reader.error);
-    reader.readAsText(blob);
-  });
-}
