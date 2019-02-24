17313dd50de70ad8f50bd90cf865f9844804e09f
kepeken <tembin0925@gmail.com>
2019-02-24 15:41:42 +0900

    FontAwesome 導入

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheck);
+
 import {
   execCopy,
   jacksonPrettyPrint,
