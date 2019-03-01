be4644261fcde924879217cfb9b3638294706d1d
kepeken <tembin0925@gmail.com>
2019-03-01 22:37:39 +0900

    Font Awesome の設定を移動

---
+++ src/js/icons.ts
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faCheck,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faCheck,
+);
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCheck);
+import './icons';
 
 import {
   execCopy,
