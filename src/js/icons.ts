6d2175b158780f11d5c3f8ab0c0a457889469db3
kepeken <tembin0925@gmail.com>
2019-03-03 19:38:15 +0900

    アイコン追加

--- src/js/icons.ts
+++ src/js/icons.ts
@@ -1,16 +1,20 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faCheck,
+  faChevronLeft,
   faLongArrowAltDown,
   faLongArrowAltUp,
   faPlus,
+  faTimes,
   faTrashAlt,
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
   faCheck,
+  faChevronLeft,
   faLongArrowAltDown,
   faLongArrowAltUp,
   faPlus,
+  faTimes,
   faTrashAlt,
 );
