6b2a033835f3cb971d27fa0bdb4065d2ff0bb24f
kepeken <tembin0925@gmail.com>
2019-03-07 01:27:14 +0900

    関数コンポーネントに書き直し

--- src/components/SearchOptionForm.tsx
+++ src/components/SearchOptionForm.tsx
@@ -6,68 +6,60 @@ interface Props {
   options: SearchOptions;
 }
 
-interface State extends SearchOptions { }
+export default function SearchOptionForm(props: Props) {
+  const [mode, setMode] = React.useState<SearchOptions["mode"]>("name");
+  const [type, setType] = React.useState<SearchOptions["type"]>("exact");
 
-export default class SearchOptionForm extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = props.options;
-  }
+  React.useEffect(() => {
+    props.onChange({ mode, type });
+  }, [mode, type]);
 
-  handleChange<K extends keyof State>(newState: Pick<State, K>) {
-    this.setState(newState);
-    this.props.onChange(Object.assign({}, this.state, newState));
-  }
-
-  render() {
-    const { mode, type } = this.state;
-    return (
-      <div className="search-option">
-        <div>
-          <label>
-            <input
-              type="radio"
-              checked={mode === "name"}
-              onChange={() => this.handleChange({ mode: "name" })}
-            />
-            単語
-          </label>
-          <label>
-            <input
-              type="radio"
-              checked={mode === "equivalent"}
-              onChange={() => this.handleChange({ mode: "equivalent" })}
-            />
-            訳語
-          </label>
-          <label>
-            <input
-              type="radio"
-              checked={mode === "content"}
-              onChange={() => this.handleChange({ mode: "content" })}
-            />
-            全文
-          </label>
-        </div>
-        <div>
-          <label>
-            <input
-              type="radio"
-              checked={type === "exact"}
-              onChange={() => this.handleChange({ type: "exact" })}
-            />
-            完全一致
-          </label>
-          <label>
-            <input
-              type="radio"
-              checked={type === "part"}
-              onChange={() => this.handleChange({ type: "part" })}
-            />
-            部分一致
-          </label>
-        </div>
+  return (
+    <div className="search-option">
+      <div>
+        <label>
+          <input
+            type="radio"
+            checked={mode === "name"}
+            onChange={() => setMode("name")}
+          />
+          単語
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={mode === "equivalent"}
+            onChange={() => setMode("equivalent")}
+          />
+          訳語
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={mode === "content"}
+            onChange={() => setMode("content")}
+          />
+          全文
+        </label>
       </div>
-    );
-  }
+      <div>
+        <label>
+          <input
+            type="radio"
+            checked={type === "exact"}
+            onChange={() => setType("exact")}
+          />
+          完全一致
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={type === "part"}
+            onChange={() => setType("part")}
+          />
+          部分一致
+        </label>
+      </div>
+    </div>
+  );
 }
--- src/components/WordListItem.tsx
+++ src/components/WordListItem.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as OTM from '../app/OTM/types';
 
 interface Props {
@@ -8,57 +8,55 @@ interface Props {
   onClick: (word: OTM.Word) => void;
 }
 
-export default class WordListItem extends React.Component<Props> {
-  render() {
-    const word = this.props.word;
-    return (
-      <div className="word-list-item">
-        <div className="word-list-item-header">
-          <button
-            className="btn-select-item"
-            onClick={() => this.props.onClick(word)}
-          >
-            {this.props.mode === "edit"
-              ? <FontAwesomeIcon icon="edit" />
-              : <FontAwesomeIcon icon="check" />
-            }
-          </button>
-          <h3 className="otm-entry-form">
-            {word.entry.form}
-            {word.tags.map((tag, idx) =>
-              <span key={idx} className="otm-tag">{tag}</span>
-            )}
-          </h3>
-        </div>
-        <div className="word-list-item-body">
-          {word.translations.map((translation, idx) =>
-            <div key={idx}>
-              <span className="otm-translation-title">{translation.title}</span>
-              {translation.forms.map((form, idx) =>
-                <span key={idx} className="otm-translation-form">{form}</span>
-              )}
-            </div>
+export default function WordListItem(props: Props) {
+  const { word } = props;
+  return (
+    <div className="word-list-item">
+      <div className="word-list-item-header">
+        <button
+          className="btn-select-item"
+          onClick={() => props.onClick(word)}
+        >
+          {props.mode === "edit"
+            ? <FontAwesomeIcon icon="edit" />
+            : <FontAwesomeIcon icon="check" />
+          }
+        </button>
+        <h3 className="otm-entry-form">
+          {word.entry.form}
+          {word.tags.map((tag, idx) =>
+            <span key={idx} className="otm-tag">{tag}</span>
           )}
-          {word.contents.map((content, idx) =>
-            <div key={idx}>
-              <div className="otm-content-title">{content.title}</div>
-              <div className="otm-content-text">{content.text}</div>
-            </div>
-          )}
-          {word.variations.map((variation, idx) =>
-            <div key={idx}>
-              <span className="otm-variation-title">{variation.title}</span>
-              <span className="otm-variation-form">{variation.form}</span>
-            </div>
-          )}
-          {word.relations.map((relation, idx) =>
-            <div key={idx}>
-              <span className="otm-relation-title">{relation.title}</span>
-              <span className="otm-relation-entry">{relation.entry.form}</span>
-            </div>
-          )}
-        </div>
+        </h3>
       </div>
-    );
-  }
+      <div className="word-list-item-body">
+        {word.translations.map((translation, idx) =>
+          <div key={idx}>
+            <span className="otm-translation-title">{translation.title}</span>
+            {translation.forms.map((form, idx) =>
+              <span key={idx} className="otm-translation-form">{form}</span>
+            )}
+          </div>
+        )}
+        {word.contents.map((content, idx) =>
+          <div key={idx}>
+            <div className="otm-content-title">{content.title}</div>
+            <div className="otm-content-text">{content.text}</div>
+          </div>
+        )}
+        {word.variations.map((variation, idx) =>
+          <div key={idx}>
+            <span className="otm-variation-title">{variation.title}</span>
+            <span className="otm-variation-form">{variation.form}</span>
+          </div>
+        )}
+        {word.relations.map((relation, idx) =>
+          <div key={idx}>
+            <span className="otm-relation-title">{relation.title}</span>
+            <span className="otm-relation-entry">{relation.entry.form}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
