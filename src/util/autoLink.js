c66de92f7e6a2833d2bd95093cb6a8ff19dd3d09
kepeken <tembin0925@gmail.com>
2019-03-10 04:51:39 +0900

    不要ファイル削除

--- src/util/autoLink.js
+++
@@ -1,5 +0,0 @@
-import escapeHTML from "./escapeHTML";
-
-export default function autoLink(string) {
-  return escapeHTML(string).replace(/https?\:\/\/\S+/g, '<a href="$&" target="_blank">$&</a>');
-}
--- src/util/escapeHTML.js
+++
@@ -1,5 +0,0 @@
-export default function escapeHTML(string) {
-  return string.replace(/[&'`"<>]/g, function (match) {
-    return { '&': '&amp;', "'": '&#x27;', '`': '&#x60;', '"': '&quot;', '<': '&lt;', '>': '&gt;' }[match];
-  });
-}
