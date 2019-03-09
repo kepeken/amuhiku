3adeae04f39ca87c9095144105eb87a6dd452a6d
kepeken <tembin0925@gmail.com>
2019-03-10 04:50:25 +0900

    source map 有効化

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,17 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
   },
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: "awesome-typescript-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
       },
       {
         test: /\.html$/,
