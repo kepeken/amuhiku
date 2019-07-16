f4ceed2fa2269034b94a6b495221c5f3c59d51ad
kepeken <tembin0925@gmail.com>
2019-07-16 18:16:01 +0900

    Add readAsArrayBuffer

---
+++ src/util/readAsArrayBuffer.ts
@@ -0,0 +1,8 @@
+export default function readAsArrayBuffer(blob: Blob) {
+  return new Promise<ArrayBuffer>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result as ArrayBuffer);
+    reader.onerror = () => reject(reader.error);
+    reader.readAsArrayBuffer(blob);
+  });
+}
--- src/util/readAsText.ts
+++ src/util/readAsText.ts
@@ -1,9 +1,6 @@
 export default function readAsText(blob: Blob) {
   return new Promise<string>((resolve, reject) => {
     const reader = new FileReader();
-    // FileReader.result の型は string | ArrayBuffer | null だが、
-    // readAsText を呼んでいるため成功時は string であることが保証されるはず。
-    // 詳しくは https://w3c.github.io/FileAPI/#dfn-result を参照。
     reader.onload = () => resolve(reader.result as string);
     reader.onerror = () => reject(reader.error);
     reader.readAsText(blob);
